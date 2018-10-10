80254f24456f19b6a608aca3c46343021611d411
Eva Spessotto <eva@spessotto.com>
2018-10-10 11:46:16 +0200

    Creation branche login_page

---
+++ front/src/components/LoginPage.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from 'react';
+import Navbar from './Navbar'
+
+class LoginPage extends Component {
+    render() {
+        return (
+            <Fragment>
+                <Navbar />
+            </Fragment>
+        );
+    }
+}
+
+export default LoginPage;
