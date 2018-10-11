69a4418f65ddb6ac970d5d508ede7849fb227816
Jules Grenier <grenier.jules@outlook.com>
2018-10-11 19:28:20 +0200

    Installation mdbreact

--- front/src/components/Loading.js
+++ front/src/components/Loading.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import logo from '../img/logo.png'
+import logo from '../img/logo.png';
+import '../index.css';
 
 class Loading extends Component {
     render() {
