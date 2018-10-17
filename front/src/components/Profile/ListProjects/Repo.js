550c19c1abfe8c6a07904eb132ac19714ca5c78e
Nelson Hui <nelson.hui123@gmail.com>
2018-10-17 10:32:15 +0200

    Séparation de la page Repo en RepoDescription + clean console

--- front/src/components/Profile/ListProjects/Repo.js
+++ front/src/components/Profile/ListProjects/Repo.js
@@ -1,107 +1,19 @@
-import React, { Component } from 'react';
-import token from '../../../config';
-import ReactTooltip from 'react-tooltip';
-import Raw from './DisplayCode/Raw';
-import { Container, Row, Col, Card, CardTitle, CardBody, CardFooter, Button, Fa } from 'mdbreact';
+import React, { Component, Fragment } from 'react';
+import RepoDescription from './RepoDescription';
+import Navbar from '../../Navbar';
+import Footer from '../../Footer';
 
 class Repo extends Component {
 
-  constructor(props){
-    super(props);
-    this.state = {
-      repo: {},
-      files: [],
-    }
-  }
-
-  componentDidMount() {
-    const { ownerName, repoName } = this.props.match.params;
-    fetch(`https://api.github.com/repos/${ownerName}/${repoName}`, {
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
-    })
-      .then(results  =>  results.json())
-        .then(repo => {
-          this.setState({
-            repo: repo
-          })
-        });
-        fetch(`https://api.github.com/repos/${ownerName}/${repoName}/contents`, {
-          headers: {
-            Authorization: `Bearer ${token}`
-          }
-        })
-          .then(results  =>  results.json())
-          .then(files => {
-            this.setState({
-              files: files
-            })
-          })
-  }
-
   render() {
-    const { repoName } = this.props.match.params;
-    const { name, html_url, description, created_at, updated_at, url } = this.state.repo;
-    const readmeObj = this.state.files.filter(x=>x.name==='README.md')
+    const { ownerName, repoName } = this.props.match.params;
     return (
-      <Container>
-
-        <Row>
-
-          <Col>
-
-            <Card>
-
-              <CardTitle>
-                <div>{ name }</div>
-              </CardTitle>
-
-              <CardBody>
-                <a
-                  href={ html_url }
-                  target='_blank'
-                  rel="noopener noreferrer"
-                  className="ghIcon"
-                  data-tip data-for={`tip-repo-1`}
-                >
-                  <Fa icon="github"/>
-                </a>
-                <ReactTooltip
-                  id={`tip-repo-1`}
-                  place="left"
-                  type="dark"
-                  effect="solid"
-                >
-                  Voir dans GitHub
-                </ReactTooltip>
-                <div>{ description }</div>
-
-
-                {/*Affichage du README RAW COLOR SYNTAX*/}
-                {
-                  this.state.files.length &&
-                  <Raw readmeObj={readmeObj[0]}/>
-                }
-
-
-              </CardBody>
-
-              <CardFooter>
-                <div>
-                  { created_at }
-                  { updated_at }
-                </div>
-              </CardFooter>
-
-            </Card>
-
-          </Col>
-
-        </Row>
-
-      </Container>
-    );
+      <Fragment>
+        <Navbar/>
+        <RepoDescription ownerName={ownerName} repoName={repoName}/>
+        <Footer/>
+      </Fragment>
+    )
   }
 }
 
