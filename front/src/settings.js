82aeff762d87d9df703ff91b9e386c2da595aa98
Benoît Hubert <benoithubert@gmail.com>
2018-12-08 23:52:26 +0100

    Fix settings front

--- front/src/settings.js
+++ front/src/settings.js
@@ -1,5 +1,5 @@
 export const token = '6915ea5a6bdf7bfb42a54a7e66c9d42ee1c74ef6';
 
-export const clientId = '24742bae17fe39a3e930';
-export const redirectUri = 'http://localhost:3000';
+export const clientId = '5d0270145c9c0079e493';
+export const redirectUri = 'http://wildhub.wild31.com';
 export const apiUrl = 'https://wildhub.wild31.com';
